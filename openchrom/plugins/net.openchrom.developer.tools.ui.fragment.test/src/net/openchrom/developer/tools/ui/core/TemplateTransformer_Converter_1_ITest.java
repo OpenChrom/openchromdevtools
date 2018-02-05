dd01f86cbe47553acf06583110bba14b4fc7e21b
Dr. Philip Wenig <philip.wenig@openchrom.net>
2018-02-05 10:38:06 +0100

    The templates have been updated.

--- openchrom/plugins/net.openchrom.developer.tools.ui.fragment.test/src/net/openchrom/developer/tools/ui/core/TemplateTransformer_Converter_1_ITest.java
+++ openchrom/plugins/net.openchrom.developer.tools.ui.fragment.test/src/net/openchrom/developer/tools/ui/core/TemplateTransformer_Converter_1_ITest.java
@@ -48,19 +48,27 @@ public class TemplateTransformer_Converter_1_ITest extends TestCase {
 		 */
 		StringBuilder license = new StringBuilder();
 		license.append("/*******************************************************************************\n");
-		license.append(" * Copyright (c) 2017, 2018 Lablicate GmbH.\n");
+		license.append(" * Copyright (c) 2018 Lablicate GmbH.\n");
 		license.append(" *\n");
-		license.append(" * All rights reserved. This program and the accompanying materials\n");
-		license.append(" * are made available under the terms of the Eclipse Public License v1.0\n");
-		license.append(" * which accompanies this distribution, and is available at\n");
-		license.append(" * http://www.eclipse.org/legal/epl-v10.html\n");
+		license.append(" * All rights reserved. This program is free software: you can redistribute it and/or modify\n");
+		license.append(" * it under the terms of the GNU Affero General Public License, version 3,\n");
+		license.append(" * as published by the Free Software Foundation.\n");
+		license.append(" *\n");
+		license.append(" * This program is distributed in the hope that it will be useful,\n");
+		license.append(" * but WITHOUT ANY WARRANTY; without even the implied warranty of\n");
+		license.append(" * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the\n");
+		license.append(" * GNU Affero General Public License for more details.\n");
+		license.append(" *\n");
+		license.append(" * You should have received a copy of the GNU Affero General Public License\n");
+		license.append(" * along with this program. If not, see <http://www.gnu.org/licenses/>.\n");
+		license.append(" *\n");
 		license.append(" * Contributors:\n");
 		license.append(" *\n");
 		license.append(" * Dr. Philip Wenig - initial API and implementation\n");
 		license.append(" *******************************************************************************/");
 		//
 		BundleInfo bundleInfo = new BundleInfo();
-		bundleInfo.setVersion("1.2.0");
+		bundleInfo.setVersion("1.3.0");
 		bundleInfo.setVendor("OpenChrom");
 		bundleInfo.setDescription("This converter reads CAMAG chromatograms.");
 		bundleInfo.setWebsite("http://www.openchrom.net");
